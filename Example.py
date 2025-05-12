50cfb9b3a9a8998673b4c25ab96f3fcaa66975de
Sina Parsa <Sina.P64@Outlook.com>
2025-05-12 22:29:08 +0330

    Add files via upload

---
+++ Example.py
@@ -0,0 +1,27 @@
+## Laver alle små bogstaver om til stor bogstaver, her bruges .upper()
+
+first_name = "thomas"
+
+last_name = "jensen"
+
+NameCourse = "python"
+
+print(first_name.upper())
+
+print(last_name.upper())
+
+print(NameCourse.upper())
+
+## Laver alle stor bogstaver om til små bogstaver, her bruges .lower()
+
+first_name = "THOMAS"
+
+last_name = "JENSEN"
+
+NameCourse = "PYTHON"
+
+print(first_name.lower())
+
+print(last_name.lower())
+
+print(NameCourse.lower())
